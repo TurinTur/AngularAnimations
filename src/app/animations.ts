0fe24d04832d9ac9cfe79e11aa3102a1231d73cf
Turin <josedavidmoreno@hotmail.com>
2018-05-27 14:05:20 +0200

    199 parameterizing animations

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,5 @@
-import { trigger, transition, style, animate, state } from "@angular/animations";
+import { trigger, transition, style, animate, state, keyframes, animation, useAnimation } from "@angular/animations";
+                                                // OT: Hay una libreria llamada animate.css con efectos predefinidos bastante útil
 
 export let fade =
     trigger('fade', [                       // nombre de trigger, array
@@ -31,5 +32,36 @@ export let fadeState2 =
       //transition('void <=> *', [                        // Lo mismo pero con otra notación
            animate( 2000)      
       ])
-  ]) ;                  
+  ]) ;       
+  
+export let bounceOutLeftAnimation = animation(            
+    animate('1000ms 0.1s ease-out', keyframes([        // KEYFRAMES
+        style({
+            offset: 0.2,                                // Keyframe 1, al 20% de la animación
+            opacity: 1,
+            transform: 'translateX(30px)'
+        }),
+        style({
+            offset: 1,                                  // key fram 2, al final de la animación (100%)
+            opacity: 0,
+            transform: 'translateX(-100%)'
+        })
+    ])) 
+);
+
+export let slide = trigger('slide', [
+    transition(':enter', [
+        style ({ transform: 'translateX(-10px)'}),          // cuando el item aparece en pantalla, esta ligeramente a la izquierda, y se coloca en su sitio
+        animate(500)
+    ]),
+    /*transition(':leave', [
+        //animate('500ms 0.1s ease-in', style ({ transform: 'translateX(-100%)'})) // cuando el item desaparece, se va totalmente a la izquierda
+        // la duración tiene parametros extra si se usa un string: duracion, delay, easing.
+        // easings: linear, ease-in, ease-out, ease-in-out. Usar http://cubic-bezier.com para custom. cubic-bezier(.17,.66,.9,.58)
+        animate('500ms cubic-bezier(.17,.66,.9,.58)', style ({ transform: 'translateX(-100%)'}))
+    ])*/
+    transition(':leave', [                                   
+       useAnimation(bounceOutLeftAnimation)
+    ])
+])
   
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,19 +1,34 @@
-import { fade, fadeState, fadeState2 } from './../animations';
+import { fade, fadeState, fadeState2, slide, bounceOutLeftAnimation } from './../animations';
 import { Component } from '@angular/core';
-import { trigger, state, transition, animate, style } from '@angular/animations';   // Importar de /animations, no de /core
+import { trigger, state, transition, animate, style, useAnimation } from '@angular/animations';   // Importar de /animations, no de /core
 
 @Component({
   selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
-  animations: [                             // Se ha movido todo a animation.ts
+  animations: [                               // Se ha movido todo a animation.ts
     /*trigger('fade', [                       // nombre de trigger, array
       ]),
-    trigger('fade-state',[                // Ahora lo mismo pero  usando estados
+    trigger('fade-state',[                    // Ahora lo mismo pero  usando estados
       ]),
-    trigger('fade-state2',[                // Mas abreviado
+    trigger('fade-state2',[                   // Mas abreviado
       ])  */     
-      fade,fadeState,fadeState2            
+      fade,fadeState,fadeState2, slide,
+      trigger('todoAnimation',[
+          transition(':enter', [
+            style ({ opacity: 0}),              // cuando el item aparece en pantalla, esta ligeramente a la izquierda, y se coloca en su sitio
+            animate(2000)
+            ]),
+        transition(':leave', [
+          style ({ backgroundColor:'red'}),         // primero pasa de rojo a 'default' (blanco) en 1000ms, despues hace la animación  
+          animate(1000),
+          /*style ({ transform: 'translateX(-1000px)'}),    // puedes hacer varias animaciones una tras otra        
+          animate(2000),
+          style ({ transform: 'translateX(1000px)'}),          
+          animate(2000)*/
+          useAnimation(bounceOutLeftAnimation)      // reusamos una animación predefinida, por no repetir codigo
+           ])
+      ])           
   ]
 })
 export class TodosComponent {
